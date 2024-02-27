c2f7e6fe499d543682066e76efc6b0949c8d07e0
Cédric Le Goater <clg@redhat.com>
2024-02-27 19:03:35 +0100

    migration: Fix migration termination
    Handle migration termination when in SETUP state. This can happen if
    qemu_savevm_state_setup() fails.
    
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- migration/migration.c
+++ migration/migration.c
@@ -3210,6 +3210,8 @@ static void migration_iteration_finish(MigrationState *s)
             }
         }
         break;
+    case MIGRATION_STATUS_SETUP:
+        break;
 
     default:
         /* Should not reach here, but if so, forgive the VM. */
@@ -3237,6 +3239,8 @@ static void bg_migration_iteration_finish(MigrationState *s)
     case MIGRATION_STATUS_FAILED:
     case MIGRATION_STATUS_CANCELLED:
     case MIGRATION_STATUS_CANCELLING:
+        break;
+    case MIGRATION_STATUS_SETUP:
         break;
 
     default:
