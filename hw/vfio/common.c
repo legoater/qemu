adb8d0441962c19b64e1c7e74e3357e5afb55012
Cédric Le Goater <clg@redhat.com>
2025-03-26 08:51:14 +0100

    vfio: Rename vfio_devices_all_dirty_tracking_started()
    Also rename vfio_devices_all_device_dirty_tracking_started() while at
    it and use the prefix 'vfio_container_devices_' for routines simply
    looping over the container's device list.
    
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>
    Reviewed-by: John Levon <john.levon@nutanix.com>
    Link: https://lore.kernel.org/qemu-devel/20250326075122.1299361-30-clg@redhat.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- hw/vfio/common.c
+++ hw/vfio/common.c
@@ -56,7 +56,7 @@ static bool vfio_log_sync_needed(const VFIOContainerBase *bcontainer)
 {
     VFIODevice *vbasedev;
 
-    if (!vfio_devices_all_dirty_tracking_started(bcontainer)) {
+    if (!vfio_container_dirty_tracking_is_started(bcontainer)) {
         return false;
     }
 
