42becc6a9b7830b3573ff6b74c7499e3bdfc182e
Zhenzhong Duan <zhenzhong.duan@intel.com>
2025-11-17 04:37:12 -0500

    intel_iommu_accel: Check for compatibility with IOMMUFD backed device when x-flts=on
    When vIOMMU is configured x-flts=on in scalable mode, first stage page table
    is passed to host to construct nested page table for passthrough devices.
    
    We need to check compatibility of some critical IOMMU capabilities between
    vIOMMU and host IOMMU to ensure guest first stage page table could be used by
    host.
    
    For instance, vIOMMU supports first stage 1GB large page mapping, but host does
    not, then this IOMMUFD backed device should fail.
    
    Even of the checks pass, for now we willingly reject the association because
    all the bits are not there yet, it will be relaxed in the end of this series.
    
    Note vIOMMU has exposed IOMMU_HWPT_ALLOC_NEST_PARENT flag to force VFIO core to
    create nesting parent HWPT, if host doesn't support nested translation, the
    creation will fail. So no need to check nested capability here.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Link: https://lore.kernel.org/qemu-devel/20251117093729.1121324-10-zhenzhong.duan@intel.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

---
+++ hw/i386/intel_iommu_accel.h
@@ -0,0 +1,28 @@
+/*
+ * Intel IOMMU acceleration with nested translation
+ *
+ * Copyright (C) 2025 Intel Corporation.
+ *
+ * Authors: Zhenzhong Duan <zhenzhong.duan@intel.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_I386_INTEL_IOMMU_ACCEL_H
+#define HW_I386_INTEL_IOMMU_ACCEL_H
+#include CONFIG_DEVICES
+
+#ifdef CONFIG_VTD_ACCEL
+bool vtd_check_hiod_accel(IntelIOMMUState *s, HostIOMMUDevice *hiod,
+                          Error **errp);
+#else
+static inline bool vtd_check_hiod_accel(IntelIOMMUState *s,
+                                        HostIOMMUDevice *hiod,
+                                        Error **errp)
+{
+    error_setg(errp,
+               "host IOMMU is incompatible with guest first stage translation");
+    return false;
+}
+#endif
+#endif
