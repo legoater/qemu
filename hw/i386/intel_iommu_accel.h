52a66e386ae6c12d65b827a6fb19c2cec828ff81
Zhenzhong Duan <zhenzhong.duan@intel.com>
2025-11-17 04:37:23 -0500

    intel_iommu_accel: Implement get_host_iommu_quirks() callback
    Implement get_host_iommu_quirks() callback to retrieve the vendor specific
    hardware information data and convert it into bitmaps defined with enum
    host_iommu_quirks. It will be used by VFIO in subsequent patch.
    
    Suggested-by: Eric Auger <eric.auger@redhat.com>
    Suggested-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Link: https://lore.kernel.org/qemu-devel/20251117093729.1121324-21-zhenzhong.duan@intel.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- hw/i386/intel_iommu_accel.h
+++ hw/i386/intel_iommu_accel.h
@@ -20,6 +20,7 @@ bool vtd_bind_guest_pasid(VTDAddressSpace *vtd_as, Error **errp);
 void vtd_flush_host_piotlb_all_locked(IntelIOMMUState *s, uint16_t domain_id,
                                       uint32_t pasid, hwaddr addr,
                                       uint64_t npages, bool ih);
+void vtd_iommu_ops_update_accel(PCIIOMMUOps *ops);
 #else
 static inline bool vtd_check_hiod_accel(IntelIOMMUState *s,
                                         VTDHostIOMMUDevice *vtd_hiod,
@@ -44,6 +45,10 @@ static inline void vtd_flush_host_piotlb_all_locked(IntelIOMMUState *s,
                                                     uint16_t domain_id,
                                                     uint32_t pasid, hwaddr addr,
                                                     uint64_t npages, bool ih)
+{
+}
+
+static inline void vtd_iommu_ops_update_accel(PCIIOMMUOps *ops)
 {
 }
 #endif
