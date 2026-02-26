4a5986d78b9ebb3f49408214e00432b11078d0a1
Nicolin Chen <nicolinc@nvidia.com>
2026-02-26 10:50:35 +0000

    hw/arm/tegra241-cmdqv: Implement CMDQV init
    Tegra241 CMDQV extends SMMUv3 with support for virtual command queues
    (VCMDQs) exposed via a CMDQV MMIO region. The CMDQV MMIO space is split
    into 64KB pages:
    
    0x00000: Global CMDQV registers
    0x10000: Global VCMDQ registers, Page0
    0x20000: Global VCMDQ registers, Page1
    0x30000: VINTF0 logical VCMDQ registers, Page0
    0x40000: VINTF0 logical VCMDQ registers, Page1
    
    This patch wires up the Tegra241 CMDQV init callback and allocates
    vendor-specific CMDQV state. The state pointer is stored in
    SMMUv3AccelState for use by subsequent CMDQV operations.
    
    The CMDQV MMIO region and a dedicated IRQ line are registered with the
    SMMUv3 device. The MMIO read/write handlers are currently stubs and will
    be implemented in later patches.
    
    The CMDQV interrupt is edge-triggered and indicates VCMDQ or VINTF
    error conditions. This patch only registers the IRQ line. Interrupt
    generation and propagation to the guest will be added in a subsequent
    patch.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Shameer Kolothum <skolothumtho@nvidia.com>
    Link: https://lore.kernel.org/qemu-devel/20260226105056.897-12-skolothumtho@nvidia.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- hw/arm/tegra241-cmdqv.h
+++ hw/arm/tegra241-cmdqv.h
@@ -15,6 +15,24 @@
 #define TEGRA241_CMDQV_MAX_CMDQ            (1U << TEGRA241_CMDQV_NUM_CMDQ_LOG2)
 #define TEGRA241_CMDQV_NUM_SID_PER_VM_LOG2 4
 
+/*
+ * Tegra241 CMDQV MMIO layout (64KB pages)
+ *
+ * 0x00000  TEGRA241_CMDQV_CFG    (Global CMDQV configuration)
+ * 0x10000  TEGRA241_VCMDQ_PAGE0  (Virtual CMDQ page 0)
+ * 0x20000  TEGRA241_VCMDQ_PAGE1  (Virtual CMDQ page 1)
+ * 0x30000  TEGRA241_VINTF0_PAGE0 (Virtual interface 0, page 0)
+ * 0x40000  TEGRA241_VINTF0_PAGE1 (Virtual interface 0, page 1)
+ */
+#define TEGRA241_CMDQV_IO_LEN 0x50000
+
+typedef struct Tegra241CMDQV {
+    struct iommu_viommu_tegra241_cmdqv cmdqv_data;
+    SMMUv3AccelState *s_accel;
+    MemoryRegion mmio_cmdqv;
+    qemu_irq irq;
+} Tegra241CMDQV;
+
 const SMMUv3AccelCmdqvOps *tegra241_cmdqv_get_ops(void);
 
 #endif /* HW_ARM_TEGRA241_CMDQV_H */
