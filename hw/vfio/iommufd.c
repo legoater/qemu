f981f82853ebc0760ae16c288de5b01fdbfd804d
Nicolin Chen <nicolinc@nvidia.com>
2026-02-26 10:50:25 +0000

    backends/iommufd: Update iommufd_backend_get_device_info
    The updated IOMMUFD uAPI introduces the ability for userspace to request
    a specific hardware info data type via IOMMU_GET_HW_INFO. Update
    iommufd_backend_get_device_info() to set IOMMU_HW_INFO_FLAG_INPUT_TYPE
    when a non-zero type is supplied, and adjust all callers to pass a type
    value explicitly initialised to zero (IOMMU_HW_INFO_TYPE_DEFAULT) when
    no specific type is requested.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Shameer Kolothum <skolothumtho@nvidia.com>
    Link: https://lore.kernel.org/qemu-devel/20260226105056.897-2-skolothumtho@nvidia.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- hw/vfio/iommufd.c
+++ hw/vfio/iommufd.c
@@ -352,7 +352,7 @@ static bool iommufd_cdev_autodomains_get(VFIODevice *vbasedev,
     ERRP_GUARD();
     IOMMUFDBackend *iommufd = vbasedev->iommufd;
     VFIOContainer *bcontainer = VFIO_IOMMU(container);
-    uint32_t type, flags = 0;
+    uint32_t type = IOMMU_HW_INFO_TYPE_DEFAULT, flags = 0;
     uint64_t hw_caps;
     VendorCaps caps;
     VFIOIOASHwpt *hwpt;
@@ -941,7 +941,7 @@ static bool hiod_iommufd_vfio_realize(HostIOMMUDevice *hiod, void *opaque,
     HostIOMMUDeviceIOMMUFD *idev;
     HostIOMMUDeviceCaps *caps = &hiod->caps;
     VendorCaps *vendor_caps = &caps->vendor_caps;
-    enum iommu_hw_info_type type;
+    uint32_t type = IOMMU_HW_INFO_TYPE_DEFAULT;
     uint8_t max_pasid_log2;
     uint64_t hw_caps;
 
