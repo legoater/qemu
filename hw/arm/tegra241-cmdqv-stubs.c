36947100e111f03a1b4aa8c40922a9e830089db3
Shameer Kolothum <skolothumtho@nvidia.com>
2026-02-26 10:50:30 +0000

    hw/arm/tegra241-cmdqv: Add Tegra241 CMDQV ops backend stub
    Introduce a Tegra241 CMDQV backend that plugs into the SMMUv3 accelerated
    CMDQV ops interface.
    
    This patch wires up the Tegra241 CMDQV backend and provides a stub
    implementation for CMDQV probe, initialization, vIOMMU allocation
    and reset handling.
    
    Functional CMDQV support is added in follow-up patches.
    
    Signed-off-by: Shameer Kolothum <skolothumtho@nvidia.com>
    Link: https://lore.kernel.org/qemu-devel/20260226105056.897-7-skolothumtho@nvidia.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

---
+++ hw/arm/tegra241-cmdqv-stubs.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved
+ *
+ * Stubs for Tegra241 CMDQ-Virtualiisation extension for SMMUv3
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "qemu/osdep.h"
+#include "hw/arm/smmuv3.h"
+#include "smmuv3-accel.h"
+#include "hw/arm/tegra241-cmdqv.h"
+
+const SMMUv3AccelCmdqvOps *tegra241_cmdqv_get_ops(void)
+{
+    return NULL;
+}
+
